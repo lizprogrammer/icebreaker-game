4c0ddd926465458424f1a363e0ab4978a97ec914
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2025-05-20 14:13:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import uuid
 from supabase import create_client, Client
-
 st.set_page_config(page_title="Icebreaker Game", layout="centered")
 
 # Load Supabase credentials from secrets
