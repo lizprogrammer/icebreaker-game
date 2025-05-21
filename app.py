6399c371fb4c5e8d6b53578151a98cb9169f35ee
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2025-05-21 16:28:15 -0400

    Snowflake Banner

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from supabase import create_client, Client
 
 # ✅ Set page config FIRST before any other Streamlit commands
 st.set_page_config(page_title="Icebreaker Game", layout="centered")
+st.image("https://res.cloudinary.com/startup-grind/image/upload/c_scale,w_2560/c_crop,h_640,w_2560,y_0.0_mul_h_sub_0.0_mul_640/c_crop,h_640,w_2560/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-snowflake/event_banners/User%20Groups%20Filler%20Banner_yAwMUPz.png", use_column_width=True)
 
 # Load Supabase credentials
 url = st.secrets["SUPABASE_URL"]
